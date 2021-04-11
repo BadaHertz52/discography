6765b8c091e3f2ae1b6111b92118ce4afbd31bb3
BadaHertz52 <lmlwpeace@naver.com>
2021-04-11 17:23:23 +0900

    Fix error
    slider 의 변수명에 $ 붙임

--- assets/js/discography.js
+++ assets/js/discography.js
@@ -54,10 +54,10 @@ toogleHeaderNavbar ();
 function ShowLyrics () {
     
   // trackList 와 lyric 배열 만들기 
-  const trackLists = document.querySelectorAll(".tracklist .toggle_btn");
-  const arrayTrackLists = Array.from(trackLists);
-  const lyrics = document.querySelectorAll(".lyric") ;
-  const arrayLyrics = Array.from(lyrics);
+  const $trackLists = document.querySelectorAll(".tracklist .toggle_btn");
+  const $arrayTrackLists = Array.from($trackLists);
+  const $lyrics = document.querySelectorAll(".lyric") ;
+  const $arrayLyrics = Array.from($lyrics);
   const $trackToggleBtns = document.querySelectorAll('.toggle_btn');
   
   function addEventListener() {
@@ -69,11 +69,11 @@ function ShowLyrics () {
         closeOtherToggle();
       //버튼과 같은 index 의 가사가 보이도록 
         // btn의 index 구하기 
-        const $index = arrayTrackLists.indexOf(btn);
+        const $index = $arrayTrackLists.indexOf(btn);
         
         // btn과 같은 index 가사 보임 
-        const lyric = arrayLyrics[$index] ;
-        lyric.classList.toggle('on') ;
+        const $lyric = $arrayLyrics[$index] ;
+        $lyric.classList.toggle('on') ;
       });
       // 더블 클릭 시 해당 곡 가사 숨김처리 
         btn.addEventListener('dblclick',function
